066e1913b9f18ad67b9ed6c932949f6f2a0f3cbc
aa12356jm <624848668@qq.com>
2017-09-03 08:36:46 +0800

    修改训练脚本使用alexnet网络训练，忽略无用文件

--- caffe/data/plate/start_train.bat
+++ caffe/data/plate/start_train.bat
@@ -1,4 +1,4 @@
-rem  ..\..\Build\x64\Release\caffe.exe train --solver=.\AlexNet\solver.prototxt 
+..\..\Build\x64\Release\caffe.exe train --solver=.\AlexNet\solver.prototxt 
 
-..\..\Build\x64\Release\caffe.exe train --solver=.\googlenet\solver.prototxt
+rem   ..\..\Build\x64\Release\caffe.exe train --solver=.\googlenet\solver.prototxt
 pause
